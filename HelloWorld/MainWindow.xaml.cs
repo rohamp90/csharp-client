9402ceb5597101fe9efa5651c45f1f7229189f16
sabet <sabet@cstructor.com>
2018-02-27 21:14:09 -0800

    Session 1

---
+++ HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Exercise #1: Maximize the window using code
+            // use WindowState
+            // WindowState = WindowState.Maximized; // note that you can use also Minimize or Normal
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Text);
+        }
+    }
+}
